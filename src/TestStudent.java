40e906436b96654787e85568d1c560b8d8bf5cb4
mb1722 <mb1722@PC-00191517.uni.mdx.ac.uk>
2014-11-04 10:53:52 +0000

    created a new class for name, age and module

---
+++ src/TestStudent.java
@@ -0,0 +1,14 @@
+
+public class TestStudent {
+
+	public class testStudent {
+
+		public static void main(String[] args) {
+			
+			student firstStudent = new student("John", 19, "Biology");
+			firstStudent.displayStudentDetails();
+		
+			
+			
+		}
+	}
